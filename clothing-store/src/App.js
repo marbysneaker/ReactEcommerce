b75d70edf7153327e2983cfb0cdcf71c831229d0
marbypangilinan <marbysneaker@gmail.com>
2023-03-25 12:50:52 -0700

    added routes

--- clothing-store/src/App.js
+++ clothing-store/src/App.js
@@ -2,8 +2,18 @@ import { Routes, Route} from 'react-router-dom';
 
 import Home from "./routes/home/home.component";
 
+import Navigation from './routes/navigation/navigation.component';
 
 
+
+
+const Shop = () => {
+  return <h1>I am the shop page</h1>
+}
+
+const Cart = () => {
+  return <h1>I am the cart page</h1>
+}
 const App = () => {
 
 
@@ -11,7 +21,14 @@ const App = () => {
   return(
    
       <Routes>
-        <Route path='/' index element={<Home />} />
+        <Route path='/' element={<Navigation/>}>
+            <Route path = 'cart' element={<Cart/>}/>
+            <Route index element={<Home />}/>
+            <Route path='shop' element={<Shop/>} />  
+
+
+        </Route>
+      
       </Routes>
     
     
